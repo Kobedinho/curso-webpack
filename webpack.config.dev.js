7c5c58802367960f6f4ce550870adc48c1212e19
COGN AKA Kobedinho <coben.9@gmail.com>
2022-04-21 19:55:10 -0500

    Agregagndo webpack server y webpack analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const DotEnv = require("dotenv-webpack")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -11,7 +12,6 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
     resolve: {
         extensions:['.js'],
         alias:{
@@ -74,5 +74,12 @@ module.exports = {
             }]
         }),
         new DotEnv(),
+        new BundleAnalyzerPlugin()
     ],
+    devServer:{
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006
+    }
 }
